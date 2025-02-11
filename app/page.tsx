2141d7e549d8dedb8e5cbd66408dfc31176f9884
Gazi2050 <gazi45344@gmail.com>
2025-02-12 00:25:23 +0600

    config:improved eslint configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
 
-
-export default function Home() {
+const Home = () => {
   return (
     <>
-      <Button>Click me </Button>
+      <Button>Click me</Button>
     </>
   );
-}
+};
+
+export default Home;
