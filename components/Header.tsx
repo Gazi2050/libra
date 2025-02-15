b14fb89fbbd3d161a762582b0f7fab9bb40ff154
Gazi2050 <gazi45344@gmail.com>
2025-02-15 14:57:15 +0600

    assets added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,29 @@
+"use client"
+import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Header = () => {
+    const pathName = usePathname();
     return (
         <header className='my-10 flex justify-between gap-5'>
             <Link href="/">Libra</Link>
             <ul className='flex flex-row items-center gap-8'>
                 <li>
-                    <Link href='/library' className='text-base cursor-pointer capitalize'>Library</Link>
+                    <Link
+                        href='/library'
+                        className={cn(
+                            "text-base cursor-pointer capitalize",
+                            pathName === "/library" ? "text-light-200" : "text-light-100"
+                        )}
+                    >
+                        Library
+                    </Link>
                 </li>
             </ul>
         </header>
+
     );
 };
 
