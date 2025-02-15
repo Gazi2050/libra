c5bb640b93406ccf9f335b0665e017edb10360f2
Gazi2050 <gazi45344@gmail.com>
2025-02-15 20:27:33 +0600

    ui:creating BookCover

---
+++ components/BookCover.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+    className?: string;
+    variant: "wide" | "small";
+    coverColor: string;
+    coverUrl: string;
+}
+const BookCover = ({ className, variant, coverColor, coverUrl }: Props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default BookCover;
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,17 +1,16 @@
+import Image from 'next/image';
 import React from 'react';
+import { Button } from './ui/button';
+import BookCover from './BookCover';
 
 const BookOverview = ({
     title,
     author,
     genre,
     rating,
-    totalCopies,
-    availableCopies,
+    total_copies,
+    available_copies,
     description,
-    coverColor,
-    coverUrl,
-    id,
-    userId,
 }: Book) => {
     return (
         <section className='book-overview'>
@@ -21,6 +20,46 @@ const BookOverview = ({
                     <p>
                         By <span className='font-semibold text-light-200'>{author}</span>
                     </p>
+                    <p>
+                        Category{" "}
+                        <span className='font-semibold text-light-200'>{genre}</span>
+                    </p>
+                    <div className='flex flex-row gap-1'>
+                        <Image src="/icons/star.svg" alt='star' width={22} height={22}></Image>
+                        <p>{rating}</p>
+                    </div>
+                </div>
+                <div className='book-copies'>
+                    <p>
+                        Total Books: <span>{total_copies}</span>
+                    </p>
+                    <p>
+                        Available Books: <span>{available_copies}</span>
+                    </p>
+                </div>
+                <p className='book-description'>
+                    {description}
+                </p>
+                <Button className="book-overview_btn">
+                    <Image src="/icons/book.svg" alt='book' width={20} height={20}></Image>
+                    <p className='font-bebas-neue text-xl text-dark-100'>Borrow Book</p>
+                </Button>
+            </div>
+            <div className='relative flex flex-1 justify-center'>
+                <div className='relative'>
+                    <BookCover
+                        varient="wide"
+                        className="z-10"
+                        coverColor={color}
+                        coverImage={cover}
+                    />
+                    <div className='absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden'>
+                        <BookCover
+                            varient="wide"
+                            coverColor={color}
+                            coverImage={cover}
+                        />
+                    </div>
                 </div>
             </div>
         </section>
