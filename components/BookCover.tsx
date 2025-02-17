921ad3e5bfe1815f1849288071ef82f113152260
Gazi2050 <gazi45344@gmail.com>
2025-02-17 23:03:16 +0600

    ui:creating Book Cover

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
 import React from 'react';
 
 type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
@@ -22,8 +24,13 @@ const BookCover = ({
     coverUrl = "https://placehold.co/400x600.png"
 }: Props) => {
     return (
-        <div>
-            BookCover
+        <div className={cn('relative transition-all duration-300',
+            variantStyles[variant],
+            className)}>
+            Book side svg
+            <div className='absolute z-10' style={{ left: '12%', width: '87.5%', height: '88%' }}>
+                <Image src={coverUrl} alt='Book Cover' fill className='rounded-sm object-fill' />
+            </div>
         </div>
     );
 };
