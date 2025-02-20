99c4a53ba91cd6d80a4acc71ea9f1e26096a8b1f
Gazi2050 <gazi45344@gmail.com>
2025-02-20 13:22:11 +0600

    initial ui ready for bookOverview

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import React from 'react';
+import BookCoverSvg from './BookCoverSvg';
 
 type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
 
@@ -15,19 +16,19 @@ interface Props {
     className?: string;
     variant?: BookCoverVariant;
     coverColor: string;
-    coverUrl: string;
+    coverImage: string;
 }
 const BookCover = ({
     className,
     variant = "regular",
-    coverColor = "#012B48",
+    coverColor = "#1D1E40",
     coverImage = "https://placehold.co/400x600.png"
 }: Props) => {
     return (
         <div className={cn('relative transition-all duration-300',
             variantStyles[variant],
             className)}>
-            Book side svg
+            <BookCoverSvg coverColor={coverColor} />
             <div className='absolute z-10' style={{ left: '12%', width: '87.5%', height: '88%' }}>
                 <Image src={coverImage} alt='Book Cover' fill className='rounded-sm object-fill' />
             </div>
