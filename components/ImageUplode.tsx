392d2fc4f6ae513bd8457ce0130061c0fb73e52a
Gazi2050 <gazi45344@gmail.com>
2025-03-07 23:18:11 +0600

    feat:integrating imageKit

--- components/ImageUplode.tsx
+++ components/ImageUplode.tsx
@@ -1,4 +1,23 @@
-import React from 'react';
+"use client"
+import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+const authenticator = async () => {
+    try {
+        const response = await fetch(`{config.env.apiEndpoint}/api/auth/imagekit`);
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+        }
+
+        const data = await response.json();
+
+        const { signature, expire, token } = data;
+        return { signature, expire, token };
+
+    } catch (error: any) {
+        throw new Error(`Authentication request failed: ${error.message}`);
+    }
+}
 
 const ImageUpload = () => {
     return (
