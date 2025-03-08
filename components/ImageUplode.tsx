6008ec339636bb37fa20c17c3a03133a0beba0af
Gazi2050 <gazi45344@gmail.com>
2025-03-08 17:08:53 +0600

    ui:fix image height

--- components/ImageUplode.tsx
+++ components/ImageUplode.tsx
@@ -95,7 +95,7 @@ const ImageUpload = ({ onFileChange }: { onFileChange: (filePath: string) => voi
                         alt={file.filePath}
                         path={file.filePath}
                         width={500}
-                        height={500}
+                        height={300}
 
                     />
                 )
