98dea7ba3e720a46feb7ed5801e31a02ff942577
Gazi2050 <gazi45344@gmail.com>
2025-03-16 00:11:10 +0600

    feat:integreating auth.js

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
-import { Button } from "@/components/ui/button";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
 const Home = () => {
+
   return (
     <>
       <BookOverview {...sampleBooks[0]} />
